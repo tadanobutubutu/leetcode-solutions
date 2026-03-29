f11bce8e600c2352aa57c8c10e8391d2936d0477
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-03-29 18:03:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.5 MB (49.64%)

---
+++ 0225-implement-stack-using-queues/solution.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+class MyStack:
+
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+        # 新しく追加した要素を先頭に持ってくる
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
+
+    def pop(self) -> int:
+        return self.q.popleft()
+
+    def top(self) -> int:
+        return self.q[0]
+
+    def empty(self) -> bool:
+        return not self.q
+
