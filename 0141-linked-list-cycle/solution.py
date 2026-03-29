f82940944679dcedb4cfd03c9b2a4f736143212e
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-03-29 17:03:29 +0000

    Sync LeetCode submission Runtime - 37 ms (54.79%), Memory - 19.6 MB (13.86%)

---
+++ 0141-linked-list-cycle/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def hasCycle(self, head):
+        if not head:
+            return False
+        
+        slow = head
+        fast = head.next
+        
+        while fast and fast.next:
+            if slow == fast:
+                return True
+            slow = slow.next
+            fast = fast.next.next
+        
+        return False
+
