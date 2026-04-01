3798fe983ed2720b270ee76dae29e63d8b9622bd
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-04-01 18:57:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (49.92%)

---
+++ 0401-binary-watch/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        res = []
+        for h in range(12):
+            for m in range(60):
+                if (h.bit_count() + m.bit_count()) == turnedOn:
+                    res.append(f"{h}:{m:02d}")
+        return res
+
