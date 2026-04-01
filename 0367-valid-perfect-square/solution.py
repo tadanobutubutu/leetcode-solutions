12edd6f726f276705e80fc1fe3d2acf624eeaa4a
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-04-01 18:21:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (60.36%)

---
+++ 0367-valid-perfect-square/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        left, right = 1, num
+        while left <= right:
+            mid = (left + right) // 2
+            sq = mid * mid
+            if sq == num:
+                return True
+            elif sq < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False
+
