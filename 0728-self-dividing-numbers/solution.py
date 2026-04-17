c2d3cfa31057139bc801a4b98a898da3daf9024d
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-04-17 17:51:44 +0000

    Sync LeetCode submission Runtime - 3 ms (96.01%), Memory - 19.3 MB (37.97%)

---
+++ 0728-self-dividing-numbers/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        ans = []
+
+        for num in range(left, right + 1):
+            x = num
+            ok = True
+
+            while x > 0:
+                d = x % 10
+                if d == 0 or num % d != 0:
+                    ok = False
+                    break
+                x //= 10
+
+            if ok:
+                ans.append(num)
+
+        return ans
+
