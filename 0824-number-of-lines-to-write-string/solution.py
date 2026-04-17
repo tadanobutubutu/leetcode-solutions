9eb4b3ff1b0bbbbd913fd4698bba5c6005b23cd3
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-04-17 18:17:14 +0000

    Sync LeetCode submission Runtime - 47 ms (49.29%), Memory - 19.2 MB (55.60%)

---
+++ 0824-number-of-lines-to-write-string/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberOfLines(self, widths: List[int], s: str) -> List[int]:
+        lines = 1
+        width = 0
+
+        for ch in s:
+            w = widths[ord(ch) - ord('a')]
+            if width + w > 100:
+                lines += 1
+                width = w
+            else:
+                width += w
+
+        return [lines, width]
+
