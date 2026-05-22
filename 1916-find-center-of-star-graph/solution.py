8c7cf3e17def8dd3c0200ff452757de275739d87
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-05-22 23:08:25 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.2 MB (75.31%)

---
+++ 1916-find-center-of-star-graph/solution.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        if edges[0][0] in edges[1]:
+            return edges[0][0]
+        return edges[0][1]
+
