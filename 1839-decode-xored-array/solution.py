d55464344d3f3aa13e75a6844f861ae2f4649e16
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-05-22 23:05:24 +0000

    Sync LeetCode submission Runtime - 7 ms (42.51%), Memory - 21 MB (33.68%)

---
+++ 1839-decode-xored-array/solution.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        arr = [first]
+        for x in encoded:
+            arr.append(arr[-1] ^ x)
+        return arr
+
