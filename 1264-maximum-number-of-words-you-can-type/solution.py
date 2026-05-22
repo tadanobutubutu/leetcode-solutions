2f46d3dea2d1b2ec4641b12baf209e0016257b92
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-05-22 23:14:43 +0000

    Sync LeetCode submission Runtime - 3 ms (37.27%), Memory - 19.3 MB (47.18%)

---
+++ 1264-maximum-number-of-words-you-can-type/solution.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        broken_set = set(brokenLetters)
+        words = text.split()
+        count = 0
+        for word in words:
+            if not (set(word) & broken_set):
+                count += 1
+        return count
+
