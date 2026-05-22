3c53327de598d23775655cf071258d99750a195c
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-05-22 22:57:10 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.7 MB (16.54%)

---
+++ 1677-matrix-diagonal-sum/solution.py
@@ -0,0 +1,7 @@
+from typing import List
+
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        n = len(mat)
+        return sum(mat[i][i] + mat[i][n - 1 - i] for i in range(n)) - (mat[n // 2][n // 2] if n % 2 != 0 else 0)
+
