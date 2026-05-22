860b4fbef29c62c81d11574787681e7f07a3292b
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-05-22 22:57:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (59.21%)

---
+++ 1698-replace-all-s-to-avoid-consecutive-repeating-characters/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def modifyString(self, s: str) -> str:
+        res = list(s)
+        n = len(res)
+        for i in range(n):
+            if res[i] == '?':
+                for char in ('a', 'b', 'c'):
+                    prev_char = res[i - 1] if i > 0 else ''
+                    next_char = res[i + 1] if i < n - 1 else ''
+                    if char != prev_char and char != next_char:
+                        res[i] = char
+                        break
+        return "".join(res)
+
