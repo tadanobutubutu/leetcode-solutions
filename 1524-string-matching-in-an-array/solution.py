9f567cba57f76c77faee9171b33b63d0838175b1
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-05-22 22:48:59 +0000

    Sync LeetCode submission Runtime - 4 ms (27.62%), Memory - 19.2 MB (90.26%)

---
+++ 1524-string-matching-in-an-array/solution.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        ans = []
+        for w1 in words:
+            for w2 in words:
+                if w1 != w2 and w1 in w2:
+                    ans.append(w1)
+                    break
+        return ans
+
