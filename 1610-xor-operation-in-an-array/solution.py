292d98ff3969c4957fc4d2aa8adde1e5cecceade
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-05-22 22:53:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (20.08%)

---
+++ 1610-xor-operation-in-an-array/solution.py
@@ -0,0 +1,8 @@
+from typing import List
+from functools import reduce
+import operator
+
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        return reduce(operator.xor, (start + 2 * i for i in range(n)))
+
