8ab265a5640b6007e11369602ee7ac9873a247d4
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-05-22 22:50:52 +0000

    Sync LeetCode submission Runtime - 7 ms (27.43%), Memory - 19.2 MB (55.99%)

---
+++ 1542-consecutive-characters/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxPower(self, s: str) -> int:
+        max_power = 1
+        current_power = 1
+        for i in range(1, len(s)):
+            if s[i] == s[i - 1]:
+                current_power += 1
+            else:
+                max_power = max(max_power, current_power)
+                current_power = 1
+        return max(max_power, current_power)
+
