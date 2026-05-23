570273050c88e7f40d98e1cddb57ff7cb469c5d4
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-05-23 21:54:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (37.56%)

---
+++ 2159-two-out-of-three/solution.py
@@ -0,0 +1,7 @@
+from typing import List
+
+class Solution:
+    def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
+        s1, s2, s3 = set(nums1), set(nums2), set(nums3)
+        return list((s1 & s2) | (s2 & s3) | (s3 & s1))
+
