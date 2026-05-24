b53dec881e78cf1184aaef5943b6e45390c07087
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-05-24 11:36:54 +0000

    Sync LeetCode submission Runtime - 3 ms (80.11%), Memory - 19.4 MB (71.27%)

---
+++ 2585-delete-greatest-value-in-each-row/solution.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def deleteGreatestValue(self, grid: List[List[int]]) -> int:
+        for row in grid:
+            row.sort()
+        return sum(max(col) for col in zip(*grid))
+
