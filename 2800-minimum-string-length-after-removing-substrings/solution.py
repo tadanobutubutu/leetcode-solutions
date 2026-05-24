7ed2b4a17941f5ba676fc09c80813197eb554179
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-05-24 11:47:40 +0000

    Sync LeetCode submission Runtime - 7 ms (46.34%), Memory - 19.5 MB (15.74%)

---
+++ 2800-minimum-string-length-after-removing-substrings/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minLength(self, s: str) -> int:
+        stack = []
+        for char in s:
+            if char == 'B' and stack and stack[-1] == 'A':
+                stack.pop()
+            elif char == 'D' and stack and stack[-1] == 'C':
+                stack.pop()
+            else:
+                stack.append(char)
+        return len(stack)
+
