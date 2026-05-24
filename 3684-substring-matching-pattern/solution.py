a68fe997bc6be95c9a8661cf8f1febb62d09c91a
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-05-24 17:49:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (26.02%)

---
+++ 3684-substring-matching-pattern/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasMatch(self, s: str, p: str) -> bool:
+        p1, p2 = p.split('*')
+        
+        idx1 = s.find(p1)
+        if idx1 == -1:
+            return False
+            
+        idx2 = s.find(p2, idx1 + len(p1))
+        return idx2 != -1
+
