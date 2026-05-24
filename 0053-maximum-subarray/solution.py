6a857b36a99f213cbb7316e75ac8700b743ff366
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-05-24 18:27:54 +0000

    Sync LeetCode submission Runtime - 45 ms (27.20%), Memory - 31.3 MB (94.33%)

---
+++ 0053-maximum-subarray/solution.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+            
+        max_so_far = nums[0]
+        max_ending_here = nums[0]
+        
+        for i in range(1, len(nums)):
+            max_ending_here = max(nums[i], max_ending_here + nums[i])
+            max_so_far = max(max_so_far, max_ending_here)
+            
+        return max_so_far
+
