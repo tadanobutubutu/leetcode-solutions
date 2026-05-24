c557e9b0d9d2ae50f98b5ae69d6ff026046997c1
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-05-24 11:55:19 +0000

    Sync LeetCode submission Runtime - 8 ms (20.95%), Memory - 19.3 MB (70.95%)

---
+++ 3093-sum-of-values-at-indices-with-k-set-bits/solution.py
@@ -0,0 +1,6 @@
+from typing import List
+
+class Solution:
+    def sumIndicesWithKSetBits(self, nums: List[int], k: int) -> int:
+        return sum(x for i, x in enumerate(nums) if bin(i).count('1') == k)
+
