d17739317dcb52471240d21f93eb6b47a60d2622
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-05-24 11:24:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (11.38%)

---
+++ 2391-strong-password-checker-ii/solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def strongPasswordCheckerII(self, password: str) -> bool:
+        if len(password) < 8:
+            return False
+        
+        has_lower = False
+        has_upper = False
+        has_digit = False
+        has_special = False
+        special_chars = set("!@#$%^&*()-+")
+        
+        for i in range(len(password)):
+            char = password[i]
+            if char.islower():
+                has_lower = True
+            elif char.isupper():
+                has_upper = True
+            elif char.isdigit():
+                has_digit = True
+            elif char in special_chars:
+                has_special = True
+            
+            if i > 0 and password[i] == password[i - 1]:
+                return False
+                
+        return has_lower and has_upper and has_digit and has_special
+
