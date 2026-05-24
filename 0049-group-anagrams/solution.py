896db1d7d945bdceaf5730cc85be0d455d5562d2
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-05-24 18:27:11 +0000

    Sync LeetCode submission Runtime - 11 ms (84.38%), Memory - 21.9 MB (80.13%)

---
+++ 0049-group-anagrams/solution.py
@@ -0,0 +1,12 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+        for s in strs:
+            key = "".join(sorted(s))
+            anagrams[key].append(s)
+            
+        return list(anagrams.values())
+
